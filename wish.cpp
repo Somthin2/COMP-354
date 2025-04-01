ff0177201c040ae57e7b110eb7e3151861134dfe
Somthin2 <125658411+Somthin2@users.noreply.github.com>
2025-04-01 09:14:50 +0000

    checks if the command exists

--- wish.cpp
+++ wish.cpp
@@ -1,13 +1,33 @@
 #include <iostream>
+#include <unistd.h>
 
 using namespace std;
+
+
+
 
 int main(int argc, char *argv[])
 {
     // Check if no arguments are passed
     if (argc == 1)
     {
-        cout << "Just started normally" << endl;
+        cout << "wish> " ;
+        string command;
+
+        // Reads a line from stream into our String 
+        getline(cin, command);
+        
+        //If the first check comes true it wont need to do the second, but if its -1 then it will go check the second one too
+        if (access(("/bin/" + command).c_str(), X_OK) == 0 || access(("/usr/bin/" + command).c_str(), X_OK)  == 0)
+        {
+            cout<<"Command Exists !"<<endl;
+        }
+        else
+        {
+            cout<<"Wrong Input !"<<endl;
+        }
+
+        exit(0);
     }
     // Check if one or more arguments are passed
     else if (argc >= 2)
